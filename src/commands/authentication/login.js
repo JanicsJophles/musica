5e1219dc13ae0cee3eaabf81db59275e5f9821d2
JanicsJophles <krishwinrocks@gmail.com>
2024-07-09 01:37:25 -0500

    db for login

--- src/commands/authentication/login.js
+++ src/commands/authentication/login.js
@@ -1,19 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 require('dotenv').config();
+const db = require('../../database');
+
 module.exports = {
-   data: new SlashCommandBuilder()
-       .setName('login')
-       .setDescription('Authenticate with Last.fm'),
-   async execute(interaction) {
-       const discordUserId = interaction.user.id;
-       const url = process.env.URL;
-       // Construct the authentication URL
-       const authUrl = `${url}auth?user=${discordUserId}`;
+    data: new SlashCommandBuilder()
+        .setName('login')
+        .setDescription('Authenticate with Last.fm'),
+    async execute(interaction) {
+        const discordUserId = interaction.user.id;
 
+        // Check if the user is already authenticated
+        db.get('SELECT lastFmUsername FROM users WHERE discordUserId = ?', [discordUserId], (err, row) => {
+            if (err) {
+                console.error('Database error:', err);
+                interaction.reply('There was an error checking your authentication status.');
+                return;
+            }
 
-       await interaction.reply(`Please authenticate with Last.fm by clicking [here](${authUrl}).`);
+            if (row) {
+                interaction.reply(`You are already logged in as ${row.lastFmUsername}!`);
+            } else {
+                const url = process.env.URL;
+                // Construct the authentication URL
+                const authUrl = `${url}auth?user=${discordUserId}`;
 
-
-       // The actual username and session key will be saved during the callback in server.js
-   },
+                interaction.reply(`Please authenticate with Last.fm by clicking [here](${authUrl}).`);
+            }
+        });
+    },
 };
